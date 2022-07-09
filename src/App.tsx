642036593cc14763dbb1a2576e4f749c116ed8ae
ShoheiTakino <104742356+ShoheiTakino@users.noreply.github.com>
2022-07-09 11:49:44 +0900

    取得データの型定義

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,28 @@ import { useState } from "react";
 import "./styles.css";
 import { Todo } from "./Todo";
 
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
 export default function App() {
-  const [todos, setTodos] = useState<any>([]);
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
 
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((result) => {
-      setTodos(result.data);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((result) => {
+        setTodos(result.data);
+      });
   };
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
-        <Todo title={todo.title} user={todo.userid} />
+        <Todo title={todo.title} userid={todo.userId} />
       ))}
     </div>
   );
